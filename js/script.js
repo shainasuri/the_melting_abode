51d95a13ca299c6c6e1a29bceff4efbd5ba1be6d
shainasuri <suris546@newschool.edu>
2021-12-13 17:47:28 -0500

    eighth commit
    web version complete

--- js/script.js
+++ js/script.js
@@ -41,3 +41,32 @@ for (var i = 0; i < sliders.length; i++) {
 // }).mount();
 
 
+function openNav() {
+    document.querySelector('#myNav').style.width = "100%";
+  }
+
+  function closeNav() {
+      document.querySelector('#myNav').style.width = "0%";
+      document.querySelector('.content-part').style.width = "0%";
+  }
+
+
+
+
+//   // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
+
+// // Get the navbar
+// var navbar = document.getElementById("navbar");
+
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
+
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
